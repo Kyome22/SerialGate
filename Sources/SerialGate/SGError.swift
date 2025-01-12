e047444843e1357280cbcd436495f99bdaf4a563
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-01-12 16:39:30 +0900

    Make it possible to read raw data.

--- Sources/SerialGate/SGError.swift
+++ Sources/SerialGate/SGError.swift
@@ -1,24 +1,30 @@
 import Foundation
 
 public enum SGError: LocalizedError, Sendable {
-    case couldNotOpenPort(String)
+    case failedToOpenPort(String)
     case portIsNotOpen(String)
-    case couldNotClosePort(String)
-    case couldNotSetOptions(String)
-    case couldNotDecodeText(String)
+    case failedToClosePort(String)
+    case failedToSetOptions(String)
+    case failedToEncodeText(String)
+    case failedToEncodeData(String)
+    case failedToWriteData(String)
 
     public var errorDescription: String? {
         switch self {
-        case let .couldNotOpenPort(portName):
-            "Could not open port (\(portName))."
+        case let .failedToOpenPort(portName):
+            "Failed to open port (\(portName))."
         case let .portIsNotOpen(portName):
             "Port (\(portName)) is not open."
-        case let .couldNotClosePort(portName):
-            "Could not close port (\(portName))."
-        case let .couldNotSetOptions(portName):
-            "Could not set options to port (\(portName))."
-        case let .couldNotDecodeText(portName):
-            "Could not decode received data to string (port \(portName))."
+        case let .failedToClosePort(portName):
+            "Failed to close port (\(portName))."
+        case let .failedToSetOptions(portName):
+            "Failed to set options (port \(portName))."
+        case let .failedToEncodeText(portName):
+            "Failed to encode received data to string (port \(portName))."
+        case let .failedToEncodeData(portName):
+            "Failed to encode received string to data (port \(portName))."
+        case let .failedToWriteData(portName):
+            "Failed to write data (port \(portName))."
         }
     }
 }
